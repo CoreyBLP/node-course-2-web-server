459aa9e01413b841955971d4213c292648527006
Corey Callahan <corey@bottomlineperformance.com>
2018-06-19 16:44:50 -0400

    initial

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+var maintenanceMode = false;
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+
+app.use((req,res,next)=>{
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log+'\n', (err)=>{
+		if(err){
+			console.log("unable to append");
+		}
+	});
+	next();
+});
+
+app.use((req,res,next)=>{
+	if(maintenanceMode){
+		res.render('maintenance.hbs');
+	}else{
+		next();
+	}
+});
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamer', (text)=>{
+	return text.toUpperCase()
+});
+
+app.get('/', (req,res)=>{
+	//res.send('<h1>Hello Express</h1>');
+	/*res.send({
+		name:'Bubby',
+		likes:['Ghouls, Pizza']
+	});*/
+	res.render('home.hbs',{
+		pageTitle: 'Home',
+		welcomeMsg:'welcome to your doom'
+	})
+});
+
+app.get('/about',(req,res)=>{
+	//res.send('About Page');
+	res.render('about.hbs', {
+		pageTitle: 'About'
+	});
+});
+
+// /bad route - send back json data with errorMessage property
+app.get('/bad',(req,res)=>{
+	res.send({
+		errorMessage:'Oh dang! That is an error'
+	});
+});
+
+
+//listen accepts port argument
+app.listen(3000, ()=>{
+	console.log('Server running: port 3000');
+});
